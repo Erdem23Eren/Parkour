57e16e46d52b2ff2e09d3ccefe75cf78f88cfd56
Erdem23Eren <153505986+Erdem23Eren@users.noreply.github.com>
2026-05-02 15:43:41 +0300

    son güncelleme

--- 1/Game1.cs
+++ 1/Game1.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using System.Collections.Generic;
 
 namespace _1;
 
@@ -10,16 +11,12 @@ public class Game1 : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
-    
     private Matrix _cameraTransform;
     private SpriteFont _font;
     private int _score = 0;
     private Vector2 _scorePosition = new Vector2(20, 20);
-    private Vector2 _scoreOrigin;
 
-   
     private Texture2D _blockTexture;
-    private Texture2D _playerTexture;
     private Player _player;
     private Song _backgroundMusic;
 
@@ -41,22 +38,24 @@ public class Game1 : Game
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-       
         _blockTexture = Content.Load<Texture2D>("images/blok");
-        _playerTexture = Content.Load<Texture2D>("images/blok");
         _font = Content.Load<SpriteFont>("fonts/04B_30");
         _backgroundMusic = Content.Load<Song>("Audio/Ses");
 
-       
-        _player = new Player(_playerTexture, new Vector2(100, 200));
+        Texture2D idleTexture = Content.Load<Texture2D>("images/1");
 
-      
+        List<Texture2D> walkFrames = new List<Texture2D>();
+        walkFrames.Add(Content.Load<Texture2D>("images/13"));
+        walkFrames.Add(Content.Load<Texture2D>("images/14"));
+        walkFrames.Add(Content.Load<Texture2D>("images/17"));
+        walkFrames.Add(Content.Load<Texture2D>("images/18"));
+        walkFrames.Add(Content.Load<Texture2D>("images/19"));
+
+        _player = new Player(idleTexture, walkFrames, new Vector2(100, 100));
+
         MediaPlayer.Play(_backgroundMusic);
         MediaPlayer.IsRepeating = true;
         MediaPlayer.Volume = 0.5f;
-
-       
-        _scoreOrigin = new Vector2(0, _font.MeasureString("Score").Y * 0.5f);
     }
 
     protected override void Update(GameTime gameTime)
@@ -64,17 +63,12 @@ public class Game1 : Game
         if (Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
 
-        
         float groundY = _graphics.PreferredBackBufferHeight - (_blockTexture.Height * GlobalScale);
-
-        
         _player.Update(gameTime, groundY);
 
-      
-        float cameraX = -_player.Position.X + (GraphicsDevice.Viewport.Width / 2);
+        float cameraX = -_player.Position.X + (GraphicsDevice.Viewport.Width / 2f);
         _cameraTransform = Matrix.CreateTranslation(cameraX, 0, 0);
 
-        
         if (Keyboard.GetState().IsKeyDown(Keys.D))
             _score += 1;
 
@@ -85,34 +79,22 @@ public class Game1 : Game
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        
         _spriteBatch.Begin(transformMatrix: _cameraTransform);
 
         float scaledWidth = _blockTexture.Width * GlobalScale;
         float groundY = _graphics.PreferredBackBufferHeight - (_blockTexture.Height * GlobalScale);
 
-        
-        for (float x = 0; x < 10000; x += scaledWidth)
+        for (float x = -1000; x < 10000; x += scaledWidth)
         {
             _spriteBatch.Draw(_blockTexture, new Vector2(x, groundY), null, Color.White, 0f, Vector2.Zero, GlobalScale, SpriteEffects.None, 0f);
         }
 
         _player.Draw(_spriteBatch);
+
         _spriteBatch.End();
 
-      
         _spriteBatch.Begin(); 
-        _spriteBatch.DrawString(
-            _font,
-            $"Score: {_score}",
-            _scorePosition,
-            Color.White,
-            0f,
-            _scoreOrigin,
-            1.0f,
-            SpriteEffects.None,
-            0f
-        );
+        _spriteBatch.DrawString(_font, $"Score: {_score}", _scorePosition, Color.White);
         _spriteBatch.End();
 
         base.Draw(gameTime);
--- 1/Player.cs
+++ 1/Player.cs
@@ -1,67 +1,103 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace _1;
 
 public class Player
 {
-    private Texture2D _texture;
+    private List<Texture2D> _walkTextures = new List<Texture2D>();
+    private Texture2D _idleTexture;
     private Vector2 _position;
     private Vector2 _velocity;
     private bool _isJumping;
-    public Vector2 Position => _position;
-    
-    public Vector2 GetPosition()
-    {
-        return _position;
-    }
-    
-  
-    private const float Gravity = 0.5f;
-    private const float JumpStrength = -10f;
-    private const float MoveSpeed = 5f;
-    private const float Scale = 0.1f; 
 
-    public Player(Texture2D texture, Vector2 startPos)
+    private int _currentFrameIndex;
+    private double _timer;
+    private double _fps = 12;
+    private float _characterScale = 4.0f;
+    private bool _isMoving;
+
+    public Vector2 Position => _position;
+
+    public Player(Texture2D idle, List<Texture2D> walkFrames, Vector2 startPos)
     {
-        _texture = texture;
+        _idleTexture = idle;
+        _walkTextures = walkFrames;
         _position = startPos;
-        _isJumping = false;
     }
 
     public void Update(GameTime gameTime, float groundY)
     {
         var kState = Keyboard.GetState();
+        _isMoving = false;
 
-      
-        if (kState.IsKeyDown(Keys.A)) _velocity.X = -MoveSpeed;
-        else if (kState.IsKeyDown(Keys.D)) _velocity.X = MoveSpeed;
-        else _velocity.X = 0;
+        if (kState.IsKeyDown(Keys.D))
+        {
+            _velocity.X = 5f;
+            _isMoving = true;
+        }
+        else if (kState.IsKeyDown(Keys.A))
+        {
+            _velocity.X = -5f;
+            _isMoving = true;
+        }
+        else
+        {
+            _velocity.X = 0;
+        }
 
-       
         if (kState.IsKeyDown(Keys.W) && !_isJumping)
         {
-            _velocity.Y = JumpStrength;
+            _velocity.Y = -12f;
             _isJumping = true;
         }
 
-        
-        _velocity.Y += Gravity;
+        _velocity.Y += 0.5f;
         _position += _velocity;
 
-     
-        float playerBottom = _position.Y + (_texture.Height * Scale);
-        if (playerBottom >= groundY)
+        if (_position.Y >= groundY)
         {
-            _position.Y = groundY - (_texture.Height * Scale);
+            _position.Y = groundY;
             _velocity.Y = 0;
             _isJumping = false;
+        }
+
+        if (_isMoving)
+        {
+            _timer += gameTime.ElapsedGameTime.TotalSeconds;
+            if (_timer > 1.0 / _fps)
+            {
+                _currentFrameIndex++;
+                if (_currentFrameIndex >= _walkTextures.Count) _currentFrameIndex = 0;
+                _timer = 0;
+            }
+        }
+        else
+        {
+            _currentFrameIndex = 0;
         }
     }
 
     public void Draw(SpriteBatch spriteBatch)
     {
-        spriteBatch.Draw(_texture, _position, null, Color.White, 0f, Vector2.Zero, Scale, SpriteEffects.None, 0f);
+        Texture2D textureToDraw = _isMoving ? _walkTextures[_currentFrameIndex] : _idleTexture;
+        
+        Vector2 origin = new Vector2(textureToDraw.Width / 2f, textureToDraw.Height);
+
+        SpriteEffects flipEffect = _velocity.X < 0 ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
+
+        spriteBatch.Draw(
+            textureToDraw, 
+            _position, 
+            null, 
+            Color.White, 
+            0f, 
+            origin, 
+            _characterScale, 
+            flipEffect, 
+            0f
+        );
     }
 }
